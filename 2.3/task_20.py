d8a14386abb6a3b36e750a8b63bf834ac26a4ea2
Egor_Kudryavtsev_PC <egakudryavtsev@mai.education>
2024-09-29 10:53:39 +0300

    Модуль 2.3, Задание 20

--- 2.3/task_20.py
+++ 2.3/task_20.py
@@ -1,38 +1,51 @@
-from random import randint
+def hashCheck4Legit(current_block: int, prev_block: int = 0) -> bool:
+    """Проверка некоторого блока на легетимность.
 
-def hashCheck4Legit(block: int, prev_block: int = 0) -> bool:
-    """_summary_
+    Проверка осуществляется путём сравнения
+    вычисленного и представленного в блоке hash'а.
+
+    Структура блока: Bn =  Mn * 256 ** 2 + Rn * 256 + Hn:
+        Bn - блок
+        Mn - натуральное число с полезной информацией
+        Rn - случайное число от 0 до 255
+        Hn - хэш блока
 
     Args:
-        info (int): usefull digit info from 0 to 255
-        prev_block (int, optional): hash of previus block. Defaults to 0.
+        current_block (int): actual block 
+        prev_block (int, optional): previus block. Defaults to 0.
 
     Returns:
         bool: is block legit
     """
 
-    # Bn = Hn + Rn * 256 + Mn * 256^2
-    hashN = block % 256
-    if hashN >= 100:
+    prev_block_hash = prev_block % 256
+    current_block_data = current_block // (256 ** 2)
+    current_block_rand = (current_block // 256) % 256
+
+    given_current_block_hash = current_block % 256
+    real_current_block_hash = 37 * (prev_block_hash + 
+                                    current_block_rand + 
+                                    current_block_data) % 256
+
+    # Хэш блока не должен быть больше 100
+    if real_current_block_hash >= 100:
+        return False
+    if real_current_block_hash != given_current_block_hash:
         return False
     return True
-
 
 
 def main():
     n = int(input())
     hashes = [int(input()) for i in range(n)]
     last_block = 0
-    flag_block = True
 
     for index, element in enumerate(hashes):
         if not hashCheck4Legit(element, last_block):
-            print(index + 1)
-            flag_block = False
+            print(index)
             break
         last_block = element
-
-    if flag_block:
+    else:
         print(-1)
             
 
